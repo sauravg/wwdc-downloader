4b9ea9dc636dd643f9a6cc91fea71d17f14670ba
Saurav Ghosh <saurav@mbp>
2015-12-09 13:56:37 +0530

    Don't let ffmpeg use the stdin
    And let ffmpeg print stats. It gets pretty boring otherwise if your
    internet is slow.

--- wwdcVideoPDFGet-curlVersion.sh
+++ wwdcVideoPDFGet-curlVersion.sh
@@ -731,7 +731,7 @@ doGetWWDC2015 () {
                     echo "downloading ${FORMAT} Video for session ${line}: ${title_array[$line]}" 
                     # TODO: we can only download to a file with extension that ffmpeg understands. So no downloading to something with
                     # .download suffix and then renaming. Fix this by inserting some tag before the file extension
-                    ffmpeg -nostats -i "${videoURL}" -c copy "${dest_path}"
+                    ffmpeg -nostdin -i "${videoURL}" -c copy "${dest_path}"
                 fi
 
                 # downloading sample codes files
@@ -778,7 +778,7 @@ doGetWWDC2015 () {
                 echo "${dest_path} already downloaded (nothing to do!)"
             else
                 echo "downloading ${FORMAT} Video for session ${line}: ${title_array[$line]}" 
-                ffmpeg -i "${videoURL}" -c copy "${dest_path}.download" && mv "${dest_path}.download" "${dest_path}"
+                ffmpeg -nostdin -i "${videoURL}" -c copy "${dest_path}.download" && mv "${dest_path}.download" "${dest_path}"
             fi
 
             # downloading sample codes files
